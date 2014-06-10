525f213db3e1b13aa7d2fd52e08ed1e16c85cc32
Purelogiq <purelogiq@gmail.com>
2014-06-10 16:59:03 -0400

    Added initial project.

---
+++ android/src/com/purelogicapps/tapgame/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.purelogicapps.tapgame.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.purelogicapps.tapgame.TapGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new TapGame(), config);
+	}
+}
